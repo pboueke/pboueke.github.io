5af825b3c2840b31762f38eb4589950b66f85b55
Pedro Boueke <phboueke@gmail.com>
2015-10-31 16:45:09 -0200

    intersecting lines v2.3

--- pages/tests/threejs/cg01/lines.js
+++ pages/tests/threejs/cg01/lines.js
@@ -3,6 +3,7 @@
 /*jslint continue:true white:true, sloppy:true, browser:true*/
 
 //global
+var zdistance = 100;
 var viewSize = 300;
 var aspectRatio = window.innerWidth / window.innerHeight;
 var scene = new THREE.Scene();
@@ -12,70 +13,18 @@ var renderer = new THREE.WebGLRenderer({ alpha: true });
 var raycaster = new THREE.Raycaster();
 //var controls = new THREE.OrbitControls(camera, document, renderer.domElement);
 //var controls = new THREE.TrackballControls( camera, document, renderer.domElement );
-var mouse = new THREE.Vector2(), INTERSECTED, INTERSECTED_COLOR, PRESSING;
+var mouse = new THREE.Vector2(), INTERSECTED, INTERSECTED_COLOR, PRESSING, PRESSABLE;
 
 //init
 INTERSECTED = "";
 PRESSING = false;
+PRESSABLE = false;
 renderer.setClearColor(0xffffff, 0);
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.sortObjects = false;
 document.body.appendChild(renderer.domElement);
-
-//interaction
-function onWindowResize() {
-    "use strict";
-    //TODOO: fix aspect radio on resize
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-}
-
-function onDocumentMouseMove(event) {
-    //updates global mouse variable
-    "use strict";
-    var aux_object,
-        aux_line,
-        aux_handle,
-        vector = new THREE.Vector3();
-    event.preventDefault();
-    mouse.x = (event.clientX / renderer.domElement.width) * 2 - 1;
-    mouse.y = -(event.clientY / renderer.domElement.height) * 2 + 1;
-    vector.set(
-        (event.clientX / window.innerWidth) * 2 - 1,
-        -(event.clientY / window.innerHeight) * 2 + 1,
-        0.5
-    );
-    //unprojecting the mouse position on the camera like this will only work on orthogonal cameras
-    vector.unproject(camera);
-    if (INTERSECTED !== "" && PRESSING) {
-        aux_object = scene.getObjectByName(INTERSECTED);
-        aux_line = parseInt(INTERSECTED.substring(11, 12), 10);
-        aux_handle = parseInt(INTERSECTED.substring(6, 7), 10);
-        console.log(aux_handle);
-        L.points[aux_line][aux_handle][0] = vector.x;
-        L.points[aux_line][aux_handle][1] = vector.y;
-        L.simpleUpdate();
-
-    }
-}
-
-function onDocumentMouseDown() {
-    "use strict";
-    PRESSING = true;
-}
-
-function onDocumentMouseUp() {
-    "use strict";
-    PRESSING = false;
-}
-
-document.addEventListener('mousedown', onDocumentMouseDown, false);
-document.addEventListener('mouseup', onDocumentMouseUp, false);
-document.addEventListener('mousemove', onDocumentMouseMove, false);
-window.addEventListener('resize', onWindowResize, false);
 
 var random_color = function () {
     "use strict";
@@ -92,6 +41,7 @@ function Lines() {
     this.handles = [];          //threejs line objects
     this.spheres = [];          //threejs line objects
     this.line_colors = [];
+    this.intersection_colors = [];
 }
 
 Lines.prototype.getLineParameters = function (arr) {
@@ -174,7 +124,6 @@ Lines.prototype.drawLines = function () {
         handle_radius = 7,
         handle_segments = 1,
         handle_rings = 1,
-        color,
         iterator;
     if (this.line_colors.length === 0) {
         for (iterator = 0; iterator < this.points.length; iterator += 1) {
@@ -217,13 +166,13 @@ Lines.prototype.drawLines = function () {
         this.handles[iterator][0] = new THREE.Mesh(handle_geometry[iterator], handle_material[iterator]);
         this.handles[iterator][0].position.x = this.points[iterator][0][0];
         this.handles[iterator][0].position.y = this.points[iterator][0][1];
-        this.handles[iterator][0].position.z = 0;
+        this.handles[iterator][0].position.z = 5;
         this.handles[iterator][0].rotation.z += Math.PI;
         this.handles[iterator][0].name = "handle0line".concat(iterator.toString());
         this.handles[iterator][1] = new THREE.Mesh(handle_geometry[iterator], handle_material[iterator]);
         this.handles[iterator][1].position.x = this.points[iterator][1][0];
         this.handles[iterator][1].position.y = this.points[iterator][1][1];
-        this.handles[iterator][1].position.z = 0;
+        this.handles[iterator][1].position.z = 5;
         this.handles[iterator][1].name = "handle1line".concat(iterator.toString());
         scene.add(this.lines[iterator]);
         scene.add(this.handles[iterator][0]);
@@ -246,7 +195,7 @@ Lines.prototype.drawIntersections = function () {
         radius = 5,
         segments = 16,
         rings = 16,
-        iterator = 0,
+        iterator,
         jterator = 0, //parses rows
         kterator = 2, //reset iterator to new value
         current = 0;
@@ -258,6 +207,10 @@ Lines.prototype.drawIntersections = function () {
     //iterator, jterator and kterator are used to create a combination that iterates
     //the current index trought every existing intersection
     while (jterator < this.points.length - 1) {
+        if (this.intersection_colors[jterator] in window) {
+            //assign color if it did not exist
+            this.intersection_colors[jterator] = random_color();
+        }
         intersec = this.intersection([this.points[jterator], this.points[iterator]]);
         maxx1 = Math.max(this.points[jterator][0][0], this.points[jterator][1][0]);
         minx1 = Math.min(this.points[jterator][0][0], this.points[jterator][1][0]);
@@ -271,7 +224,7 @@ Lines.prototype.drawIntersections = function () {
                 ((intersec[0] < maxx2 && intersec[0] > minx2) || (intersec[1] < maxy2 && intersec[1] > miny2))) {
             geometry[current] = new THREE.SphereGeometry(radius, segments, rings);
             material[current] = new THREE.MeshBasicMaterial({
-                color: random_color(),
+                color: this.intersection_colors[jterator],
                 transparent: true,
                 opacity: 0.5
             });
@@ -328,12 +281,70 @@ Lines.prototype.update = function (z) {
     //controls.target = new THREE.Vector3(cpos[0], cpos[1], 0);
 };
 
-Lines.prototype.simpleUpdate = function (z) {
+Lines.prototype.simpleUpdate = function () {
     //update scene with new parameters
     "use strict";
     this.drawLines();
     this.drawIntersections();
     //controls.target = new THREE.Vector3(cpos[0], cpos[1], 0);
+};
+
+//mouse and window interaction
+var start = function (lines) {
+    'use strict';
+    lines.parseLineText(document.getElementById("intxt").value);
+    lines.update(zdistance);
+
+    document.getElementById("gotxt").addEventListener("click", function () {
+        lines.parseLineText(document.getElementById("intxt").value);
+        lines.update(zdistance);
+    });
+
+    document.getElementById("intxt").addEventListener("change", function () {
+        lines.parseLineText(document.getElementById("intxt").value);
+        lines.update(zdistance);
+    });
+
+    document.addEventListener('mousedown', function () {
+        PRESSING = true;
+    });
+
+    document.addEventListener('mouseup', function () {
+        PRESSING = false;
+        PRESSABLE = false;
+        INTERSECTED = "";
+    });
+    document.addEventListener('mousemove', function (event) {
+        //updates global mouse variable
+        var aux_object,
+            aux_line,
+            aux_handle,
+            vector = new THREE.Vector3();
+        event.preventDefault();
+        mouse.x = (event.clientX / renderer.domElement.width) * 2 - 1;
+        mouse.y = -(event.clientY / renderer.domElement.height) * 2 + 1;
+        vector.set(
+            (event.clientX / window.innerWidth) * 2 - 1,
+            -(event.clientY / window.innerHeight) * 2 + 1,
+            0.5
+        );
+        //unprojecting the mouse position on the camera like this will only work with orthogonal cameras
+        vector.unproject(camera);
+        if (PRESSABLE && PRESSING) {
+            aux_object = scene.getObjectByName(INTERSECTED);
+            aux_line = parseInt(INTERSECTED.substring(11, 12), 10);
+            aux_handle = parseInt(INTERSECTED.substring(6, 7), 10);
+            lines.points[aux_line][aux_handle][0] = vector.x;
+            lines.points[aux_line][aux_handle][1] = vector.y;
+            lines.simpleUpdate();
+        }
+    });
+    window.addEventListener('resize', function() {
+        //TODOO: fix aspect radio on resize
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+    });
 };
 
 var render = function () {
@@ -346,20 +357,15 @@ var render = function () {
     intersects = raycaster.intersectObjects(scene.children);
     if (INTERSECTED.substring(0, 6) === 'handle') {
         aux_object = scene.getObjectByName(INTERSECTED);
-        //console.log(aux_object);
         aux_object.material.color.setHex(INTERSECTED_COLOR);
-        INTERSECTED = "";
     }
     if (intersects.length > 0) {
-        //console.log(intersects[0]);
         aux_name = intersects[0].object.name;
         if (aux_name.substring(0, 6) === 'handle') {
+            PRESSABLE = true;
             INTERSECTED = aux_name;
             INTERSECTED_COLOR = intersects[0].object.material.color.getHex();
-            //console.log(intersects[0].object);
             intersects[0].object.material.color.setHex(0xff0000);
-        } else {
-            INTERSECTED = "";
         }
     }
     renderer.render(scene, camera);
